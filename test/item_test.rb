d643fe793bccdac172fae48987e1f9f80f2a67e8
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-10 09:55:16 -0800

    Create tests to ensure methods work

--- test/item_test.rb
+++ test/item_test.rb
@@ -1,8 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/item'
-require 'pry'
-
 
 class ItemTest < Minitest::Test
+
+  def setup
+   @item1 = Item.new({name: 'Peach', price: "$0.75"})
+   @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  def test_it_exists
+    assert_instance_of Item, @item1
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "Tomato", @item2.name
+    assert_equal 0.5, @item2.price
+  end
 end
